44505363753158e7ab61c8f3b9f436fbf02e90ed
Kristy Tien <kristy4500@gmail.com>
2021-08-10 11:12:13 -0400

    test coverage done

--- tests/test_todolist.py
+++ tests/test_todolist.py
@@ -24,6 +24,17 @@ class TestTodolist(unittest.TestCase):
 
         self.assertIsNone(list.head)
         self.assertEqual(list.size, 0)
+    
+    def test_printlist(self):
+        capturedOutput = io.StringIO()               # Create StringIO object
+        sys.stdout = capturedOutput                   #  and redirect stdout.
+        testList = todolist.LinkedList()
+        testList.insert_at_beginning("head")
+        testList.insert_at_end("end")
+        testList.printlist()
+        self.assertTrue("0 head" in capturedOutput.getvalue())
+        self.assertTrue("1 end" in capturedOutput.getvalue())
+        sys.stdout = sys.__stdout__
 
     def test_insertAtBeginning(self):
         testTask1 = "testTask1"
@@ -33,14 +44,22 @@ class TestTodolist(unittest.TestCase):
         self.assertEqual(testList.head.data, testTask1)
         testList.insert_at_beginning(testTask2)
         self.assertEqual(testList.head.data, testTask2)
-
+    
+    def test_insertAtEnd_0(self):
+        testTask1 = "testTask1"
+        testList = todolist.LinkedList()
+        testList.insert_at_end(testTask1)
+        self.assertEqual(testList.head.data, "testTask1")
+    
     def test_insertAtEnd(self):
         testTask1 = "testTask1"
+        testTask2 = "testTask2"
         testList = todolist.LinkedList()
         testList.insert_at_beginning("head")
         testList.insert_at_end(testTask1)
+        testList.insert_at_end(testTask2)
         end = self.getEnd(testList.head)
-        self.assertEqual(end.data, "testTask1")
+        self.assertEqual(end.data, "testTask2")
 
     def test_insertAtPos_error(self):
         capturedOutput = io.StringIO()               # Create StringIO object
@@ -61,13 +80,15 @@ class TestTodolist(unittest.TestCase):
         testTask1 = "testTask1"
         testTask2 = "testTask2"
         testTask3 = "testTask3"
+        testTask4 = "testTask4"
 
         testList = todolist.LinkedList()
+        testList.insert_at_beginning(testTask2)
         testList.insert_at_beginning(testTask1)
-        testList.insert_at_end(testTask3)
-        testList.insert_at_pos(1, testTask2)
+        testList.insert_at_end(testTask4)
+        testList.insert_at_pos(2, testTask3)
 
-        self.assertEqual(testList.head.next.data, testTask2)
+        self.assertEqual(testList.head.next.next.data, testTask3)
     
     # def test_insertAfterTargetValue(self):
     #     # TODO
@@ -115,13 +136,15 @@ class TestTodolist(unittest.TestCase):
         testTask1 = "testTask1"
         testTask2 = "testTask2"
         testTask3 = "testTask3"
+        testTask4 = "testTask4"
         testList = todolist.LinkedList()
+        testList.insert_at_beginning(testTask4)
         testList.insert_at_beginning(testTask3)
         testList.insert_at_beginning(testTask2)
         testList.insert_at_beginning(testTask1)
-        testList.remove_at(1)
-        targetPos = testList.index_of(testTask3)
-        self.assertEqual(targetPos, 1)
+        testList.remove_at(2)
+        targetPos = testList.index_of(testTask4)
+        self.assertEqual(targetPos, 2)
     
     def test_removeTargetValue_NoneError(self):
         capturedOutput = io.StringIO()               # Create StringIO object
@@ -130,23 +153,58 @@ class TestTodolist(unittest.TestCase):
         testList.remove_target_value(None)
         self.assertTrue("Error:" in capturedOutput.getvalue())
         sys.stdout = sys.__stdout__
-
-
-
-
-        
-        
-
     
+    def test_removeTargetValue(self):
+        testTask1 = "testTask1"
+        testTask2 = "testTask2"
+        testTask3 = "testTask3"
+        testList = todolist.LinkedList()
+        testList.insert_at_beginning(testTask3)
+        testList.insert_at_beginning(testTask2)
+        testList.insert_at_beginning(testTask1)
+        testList.remove_target_value(testTask1)
+        self.assertEqual(testList.head.data, testTask2)
 
+    def test_removeTarget_noValue(self):
+        capturedOutput = io.StringIO()               # Create StringIO object
+        sys.stdout = capturedOutput   
+        testTask1 = "testTask1"
+        testTask2 = "testTask2"
+        testTask3 = "testTask3"
+        testList = todolist.LinkedList()
+        testList.insert_at_beginning(testTask3)
+        testList.insert_at_beginning(testTask2)
+        testList.insert_at_beginning(testTask1)
+        testList.remove_target_value("testTask4")
+        self.assertEqual(testList.head.data, testTask1)
+        self.assertEqual(testList.size, 3)
+        sys.stdout = sys.__stdout__
 
+    def test_nodeAt(self):
+        testTask1 = "testTask1"
+        testTask2 = "testTask2"
+        testTask3 = "testTask3"
+        testTask4 = "testTask4"
+        testList = todolist.LinkedList()
+        testList.insert_at_beginning(testTask4)
+        testList.insert_at_beginning(testTask3)
+        testList.insert_at_beginning(testTask2)
+        testList.insert_at_beginning(testTask1)
+        target = testList.node_at(2)
+        self.assertEqual(target.data, testTask3)
 
-
-
-
-
-
-
-
-
+    def test_nodeAt_outOfRangeError(self):
+        capturedOutput = io.StringIO()               # Create StringIO object
+        sys.stdout = capturedOutput     
+        testTask1 = "testTask1"
+        testTask2 = "testTask2"
+        testTask3 = "testTask3"
+        testList = todolist.LinkedList()
+        testList.insert_at_beginning(testTask3)
+        testList.insert_at_beginning(testTask2)
+        testList.insert_at_beginning(testTask1)
+        target = testList.node_at(3)
+        self.assertTrue("Error: Index not valid" in capturedOutput.getvalue())
+        self.assertEqual(target, -1)
+        sys.stdout = sys.__stdout__
 
--- tests/test_todolistApp.py
+++ tests/test_todolistApp.py
@@ -2,7 +2,6 @@ import unittest
 import todolistApp
 import sys
 import io
-import mock
 from unittest.mock import patch
 
 
@@ -17,10 +16,19 @@ class TestTodolistApp(unittest.TestCase):
         self.assertTrue("Welcome" in capturedOutput.getvalue())
         self.assertTrue("App Ended." in capturedOutput.getvalue())
         sys.stdout = sys.__stdout__
+    
+    @patch('builtins.input', side_effect=["ABC", "QUIT"])
+    def test_invalidInput(self, mock_inputs):
+        capturedOutput = io.StringIO()               # Create StringIO object
+        sys.stdout = capturedOutput
+        app = todolistApp.TodolistApp()
+        app.start()
+        self.assertTrue("Error: Command is not valid." in capturedOutput.getvalue())
+        sys.stdout = sys.__stdout__
 
     @patch('builtins.input', side_effect=["IB,task1", "QUIT"])
     def test_insertBeginning(self, mock_inputs):
-        capturedOutput = io.StringIO()               # Create StringIO object
+        capturedOutput = io.StringIO()               
         sys.stdout = capturedOutput         
         app = todolistApp.TodolistApp()
         app.start()
@@ -29,12 +37,59 @@ class TestTodolistApp(unittest.TestCase):
     
     @patch('builtins.input', side_effect=["IB,task1", "POP", "QUIT"])
     def test_insertAndPop(self, mock_inputs):
-        capturedOutput = io.StringIO()               # Create StringIO object
+        capturedOutput = io.StringIO()               
         sys.stdout = capturedOutput         
         app = todolistApp.TodolistApp()
         app.start()
         self.assertTrue("Task  task1  is finished" in capturedOutput.getvalue())
         sys.stdout = sys.__stdout__
+    
+    @patch('builtins.input', side_effect=["IB,task1", "IE,task2", "SEARCH,task1", "QUIT"])
+    def test_insertAndSearch(self, mock_inputs):
+        capturedOutput = io.StringIO()               
+        sys.stdout = capturedOutput         
+        app = todolistApp.TodolistApp()
+        app.start()
+        self.assertTrue("Task  task1  is found at index  0" in capturedOutput.getvalue())
+        sys.stdout = sys.__stdout__
+    
+    @patch('builtins.input', side_effect=["IB,task2", "IE,task3", "IP,0,task1", "QUIT"])
+    def test_insertAtPosition(self, mock_inputs):
+        capturedOutput = io.StringIO()               
+        sys.stdout = capturedOutput         
+        app = todolistApp.TodolistApp()
+        app.start()
+        self.assertTrue("Task  task1  is inserted at index  0" in capturedOutput.getvalue())
+        sys.stdout = sys.__stdout__
+    
+    @patch('builtins.input', side_effect=["IB,task1", "IE,task2", "FIND,0", "QUIT"])
+    def test_insertAndFind(self, mock_inputs):
+        capturedOutput = io.StringIO()               
+        sys.stdout = capturedOutput         
+        app = todolistApp.TodolistApp()
+        app.start()
+        self.assertTrue("Task  task1  is found at index  0" in capturedOutput.getvalue())
+        sys.stdout = sys.__stdout__
+    
+    @patch('builtins.input', side_effect=["IB,task2", "IB,task1", "RA,0", "QUIT"])
+    def test_removeAt(self, mock_inputs):
+        capturedOutput = io.StringIO()               
+        sys.stdout = capturedOutput         
+        app = todolistApp.TodolistApp()
+        app.start()
+        self.assertTrue("Task  task1  located at index  0  is removed" in capturedOutput.getvalue())
+        sys.stdout = sys.__stdout__
+    
+    @patch('builtins.input', side_effect=["IB,task2", "IB,task1", "IS,task1,taskX", "QUIT"])
+    def test_removeAt(self, mock_inputs):
+        capturedOutput = io.StringIO()               
+        sys.stdout = capturedOutput         
+        app = todolistApp.TodolistApp()
+        app.start()
+        self.assertTrue("Task  taskX  is inserted after task  task1" in capturedOutput.getvalue())
+        sys.stdout = sys.__stdout__
+
+
 
 
 
